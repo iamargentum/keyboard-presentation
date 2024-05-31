698c3dbaf9153b5cac42e8643570660f44b1c35f
iamargentum <rajat.rj97@gmail.com>
2024-05-31 06:38:44 +0530

    implement timing features for examples

--- timers.js
+++ timers.js
@@ -1,15 +1,38 @@
-let timerMap = {};
+let intervalKeeperMap =  {};
+let intervalsThatAreDone = [];
 
 function startTimer(elementId) {
+    if(
+        Object.keys(intervalKeeperMap).includes(elementId) ||
+        intervalsThatAreDone.includes(elementId)
+    ) return;
+
     const timerElement = document.getElementById(elementId);
 
-    // setInterval(() => {
-        
-    // }, 1);
+    const intervalKeeper = setInterval(() => {
+        const timerText = timerElement.innerText;
+        const [seconds, milliseconds] = timerText.split(":");
+
+        const secondsInteger = parseInt(seconds);
+        const millisInteger = parseInt(milliseconds);
+
+        if(millisInteger === 99) {
+            timerElement.innerText = `${secondsInteger + 1}:00`;
+        } else {
+            timerElement.innerText = `${secondsInteger}:${millisInteger + 1}`;
+        }
+    }, 10);
+
+    intervalKeeperMap[elementId] = intervalKeeper;
 }
 
 function handleExampleClick() {
-
 }
 
-function stopTimer(elementId) {}
+function stopTimer(elementId) {
+    if(!Object.keys(intervalKeeperMap).includes(elementId)) return;
+    
+    clearInterval(intervalKeeperMap[elementId]);
+    delete intervalKeeperMap[elementId];
+    intervalsThatAreDone.push(elementId);
+}
